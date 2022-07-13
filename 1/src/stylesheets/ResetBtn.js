c26855844d04d0180f2bd7abed7f5095b68b58f4
Fateme Esmaili <fateme.e7896@gmail.com>
2022-07-13 18:47:59 +0430

    add files of question 1

---
+++ 1/src/stylesheets/ResetBtn.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.button `
+    background-color: #1D438A;
+    color: white;
+    display: block;
+    width: 100px;
+    height: 70px;
+    margin: 50px auto;
+    border: none;
+    font-size: 1.4rem;
+    border-radius: 5px;
+    box-shadow: -2px 7px 20px -5px #7bb6c1;
+   
+    &:hover{
+        background-color: #a0c4ff;
+        
+    }
+
+`
